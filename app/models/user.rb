c1ebf7d5a956c071babfa9b8d5396d934fe0f0a2
”suno” <stargazerastronomy2@gmail.com>
2020-03-19 14:12:20 +0000

    new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: ture
-  validates :email, presence: ture
-  
+  validates :name, presence: true, length: { maximum: 15}
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
   has_secure_password
+  validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
 end
