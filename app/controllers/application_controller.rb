288a74e75a7f8337bbfbe3bdb621a4c911032305
”suno” <stargazerastronomy2@gmail.com>
2020-03-21 04:54:59 +0000

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,14 @@
-require "./app/helpers/ApplicationHelper"
-
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
-  
-#  require ".helpers/ApplicationHelper"
-  current_user
-  logged_in?
-  
-#  def current_user
-#    @current_user ||= User.find_by(id: session[:user_id])
-#  end
+  helper_method :current_user, :logged_in?
 
-#  def logged_in?
-#    !current_user.nil?
-#  end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
+module ApplicationHelper
+end
 #module ApplicationHelper
-  class ApplicationHelper
-    def current_user
-     @current_user ||= User.find_by(id: session[:user_id])
-    end
+#  class ApplicationHelper
+#    def current_user
+#     @current_user ||= User.find_by(id: session[:user_id])
+#    end
 
-    def logged_in?
-      !current_user.nil?
-    end
-  end
+#    def logged_in?
+#      !current_user.nil?
+#    end
+#  end
 #end
