e6de83e501387567e60dbe297908e9b1dc56e077
”suno” <stargazerastronomy2@gmail.com>
2020-03-24 13:25:50 +0000

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
   helper_method :current_user, :logged_in?
+  
+  before_action :login_check, only: [:new, :edit, :update, :destroy]
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,6 +27,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+  def size_range
+    1..10.megabytes
+  end
 
   # Create different versions of your uploaded files:
   # version :thumb do
@@ -35,9 +38,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+  
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
