2068fb5da8b4a30fd9490b98e40a348e9334c72c
”suno” <stargazerastronomy2@gmail.com>
2020-03-25 23:01:08 +0000

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   helper_method :current_user, :logged_in?
   
-  before_action :login_check, only: [:new, :edit, :update, :destroy]
+#  before_action :login_check, only: [:new, :edit, :update, :destroy]
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
